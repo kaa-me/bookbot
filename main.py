4ac6cf868fb8b8b72b7dd93be971f9c4129bb351
kaa-me <gnarled-vodkas-0t@icloud.com>
2025-12-15 11:46:26 +1100

    whatever

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import get_num_words
+from stats import get_num_char 
 
 def get_book_text():
-    with open("/Volumes/Samsung_SSD/OrbStack/dev/home/dev/bookbot/bookbot/books/frankenstein.txt", "r") as f:
+    with open("./books/frankenstein.txt", "r") as f:
         file_content = f.read()
     return file_content
 
 def main():
+    book_text = get_book_text()
     get_num_words(get_book_text)
+    character_counts = get_num_char(book_text)
+    print(character_counts)
 
 
-main()
-    
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -1,7 +1,16 @@
-
-
 def get_num_words(get_book_text):
     book_text = get_book_text()
     num_words = book_text.split()
     print(f'Found {len(num_words)} total words')
+
+def get_num_char(text):
+    char_counts = {}
+    
+    for char in text:
+        lowered_char = char.lower()
+        if lowered_char in char_counts:
+            char_counts[lowered_char] += 1
+        else:
+            char_counts[lowered_char] = 1
+    return char_counts
 

6e2ea5338c39e1b8a900b8b8c5acdfe1e96bbe44
kaa-me <gnarled-vodkas-0t@icloud.com>
2025-12-29 20:58:29 +1100

    the working versiob

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from stats import get_num_words
-from stats import get_num_char 
+from stats import get_num_char, get_num_words, sort_characters
+
 
 def get_book_text():
     with open("./books/frankenstein.txt", "r") as f:
-        file_content = f.read()
-    return file_content
+        return f.read()
+
 
 def main():
     book_text = get_book_text()
-    get_num_words(get_book_text)
-    character_counts = get_num_char(book_text)
-    print(character_counts)
+
+    get_num_words(book_text)
+
+    char_counts = get_num_char(book_text)
+    sorted_chars = sort_characters(char_counts)
+
+    for item in sorted_chars:
+        print(f"'{item['char']}: {item['num']}'")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,16 +1,33 @@
-def get_num_words(get_book_text):
-    book_text = get_book_text()
-    num_words = book_text.split()
-    print(f'Found {len(num_words)} total words')
+def get_num_words(text):
+    words = text.split()
+    print(f"Found {len(words)} total words")
+
 
 def get_num_char(text):
     char_counts = {}
-    
+
     for char in text:
         lowered_char = char.lower()
         if lowered_char in char_counts:
             char_counts[lowered_char] += 1
         else:
             char_counts[lowered_char] = 1
+
     return char_counts
 
+
+def sort_characters(char_counts):
+    chars_list = []
+
+    for char, count in char_counts.items():
+        if char.isalpha():
+            chars_list.append({"char": char, "num": count})
+
+    # sort from greatest to least
+    chars_list.sort(key=get_num, reverse=True)
+
+    return chars_list
+
+
+def get_num(item):
+    return item["num"]
